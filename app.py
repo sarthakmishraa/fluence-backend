a6a42235c73ce97768c6e48480d8886749faf110
Sarthak Mishra <msarthak785@gmail.com>
2024-03-29 01:08:53 -0400

    added backend for seasonal flu model

--- app.py
+++ app.py
@@ -31,20 +31,41 @@ import pandas as pd  # Import pandas
 app = Flask(__name__)
 CORS(app)
 
-model_name = "gradient_boosting_ff_h1n1_vaccine_model.joblib"
-model = joblib.load(model_name)
+model_name_h1n1 = "gradient_boosting_ff_h1n1_vaccine_model.joblib"
+model_h1n1 = joblib.load(model_name_h1n1)
 
-@app.route('/api/process_input', methods=["POST"])
-def process_input():
+model_name_seasonal = "gradient_boosting_ff_seasonal_vaccine_model.joblib"
+model_seasonal = joblib.load(model_name_seasonal)
+
+@app.route('/api/process_input_h1n1', methods=["POST"])
+def process_input_h1n1():
     data = request.json
     input_value = data.get('input')
 
-    features_h1n1 = ['doctor_recc_h1n1', 'opinion_h1n1_risk', 'opinion_h1n1_vacc_effective', 'opinion_seas_risk', 'doctor_recc_seasonal',
-                     'opinion_seas_vacc_effective', 'health_worker', 'h1n1_concern', 'health_insurance', 'h1n1_knowledge']
+    features_h1n1 = ['doctor_recc_h1n1', 'opinion_h1n1_risk', 'opinion_h1n1_vacc_effective',
+                     'opinion_seas_risk', 'doctor_recc_seasonal', 'opinion_seas_vacc_effective',
+                     'health_worker', 'h1n1_concern', 'health_insurance', 'h1n1_knowledge']
     
     input_df = pd.DataFrame([input_value], columns=features_h1n1)
     
-    result = model.predict(input_df)
+    result = model_h1n1.predict(input_df)
+
+    return jsonify({'prediction': result.tolist()})
+
+@app.route('/api/process_input_seasonal', methods=["POST"])
+def process_input_seasonal():
+    data = request.json
+    input_value = data.get('input')
+
+    features_seasonal = ['opinion_seas_risk', 'doctor_recc_seasonal', 'opinion_seas_vacc_effective',
+                         'opinion_h1n1_risk', 'opinion_h1n1_vacc_effective', 'health_insurance',
+                         'doctor_recc_h1n1', 'chronic_med_condition', 'h1n1_concern', 'health_worker',
+                         'behavioral_touch_face']
+    
+    input_df = pd.DataFrame([input_value], columns=features_seasonal)
+    print(input_df)
+    
+    result = model_seasonal.predict(input_df)
 
     return jsonify({'prediction': result.tolist()})
 
